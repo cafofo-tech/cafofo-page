b9bb6bb6c0bda3de86f1fcdb83c4eb01a0e0b1a4
Rafael Uchoa <uchoaaa@gmail.com>
2021-08-12 15:16:22 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+// 
+// Cola ;)
+// 
+// import path from 'path'
+//
+// export default class Page {
+//   #name: string
+//   #destPath: string
+//   #sourcePath: string
+//   #data = {}
+//
+//   constructor(name: string, sourcePath: string, destPath: string) {
+//     this.#name = name
+//     this.#sourcePath = `${path.resolve(sourcePath)}`
+//     this.#destPath = `${path.resolve(destPath)}`
+//   }
+//
+//   get name() : string {
+//     return this.name
+//   }
+//
+//   create() : void {
+//     console.log('1. carregar template de:', this.#sourcePath)
+//     console.log('2. aplicar dados ao template:', this.#data)
+//     console.log('3. salvar resultado em disco em:', this.#destPath)
+//   }
+// }
